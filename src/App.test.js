59e876c083983bc16c79737936f76fb548eb39d9
Marcus Baw <marcusbaw@gmail.com>
2020-08-21 17:52:37 +0100

    add a test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders RCPCH Growth Charts text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/RCPCH Growth Charts/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/RCPCH Growth Charts/i);
+  expect(textElement).toBeInTheDocument();
 });
