6b0a76d692670980f70b33a0ed13a92e7b4faafa
Simon Chapman <eatyourpeasapps@gmail.com>
2021-01-24 14:22:01 +0000

    fix measurement method dropdown

--- src/components/MeasurementSegment.js
+++ src/components/MeasurementSegment.js
@@ -53,7 +53,7 @@ class MeasurementSegment extends Component {
         super(props)
 
         // const dummyData=[{birth_data:{birth_date:"Wed, 28 Jan 2015 00:00:00 GMT",estimated_date_delivery:null,estimated_date_delivery_string:null,gestation_days:0,gestation_weeks:40,sex:"male"},child_observation_value:{measurement_method:"height",observation_value:110},measurement_calculated_values:{centile:13,centile_band:"This height measurement is between the 9th and 25th centiles.",measurement_method:"height",sds:-1.117076305831875},measurement_dates:{chronological_calendar_age:"5 years, 10 months and 4 weeks",chronological_decimal_age:5.9110198494182065,clinician_decimal_age_comment:"Born Term. No correction necessary.",corrected_calendar_age:null,corrected_decimal_age:5.9110198494182065,corrected_gestational_age:{corrected_gestation_days:null,corrected_gestation_weeks:null},lay_decimal_age_comment:"At 40+0, your child is considered to have been born at term. No age adjustment is necessary.",observation_date:"Sat, 26 Dec 2020 00:00:00 GMT"}}]
-
+        
         this.state = {
           measurementMethod: "height",
           reference: "uk-who",
@@ -144,7 +144,22 @@ class MeasurementSegment extends Component {
 
   changeMeasurement(measurementMethod){
     // call back from MeasurementForm
-    this.setState({measurementMethod: measurementMethod})
+    switch (measurementMethod){
+      case ('height'):
+        this.setState({activeIndex: 0}) // move focus to height tab
+        break
+      case ('weight'):
+        this.setState({activeIndex: 1}) // move focus to weight tab
+        break
+      case ('bmi'):
+        this.setState({activeIndex: 2}) // move focus to bmi tab
+        break
+      case ('ofc'):
+        this.setState({activeIndex: 3}) // move focus to ofc tab
+        break
+      default:
+        return
+    }
     this.returnNewChart(
       measurementMethod,
       [],
@@ -154,7 +169,8 @@ class MeasurementSegment extends Component {
       this.state.gridlineStyle,
       this.state.centileStyle,
       this.state.measurementStyle
-    )
+      )
+      this.setState({measurementMethod: measurementMethod})
   }
 
   handleResults(results){
