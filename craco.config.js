d5a162df72cd14eb7355966f43952a1e6c35022d
Simon Chapman <eatyourpeasapps@gmail.com>
2022-02-02 21:50:33 +0000

    update dependencies

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,19 @@ const { ESLINT_MODES } = require('@craco/craco');
 
 
 module.exports = {
-  plugins: [{ plugin: require("@semantic-ui-react/craco-less") }],
+  plugins: [
+    { 
+      plugin: require("@semantic-ui-react/craco-less"),
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            math: "always",
+            javascriptEnabled: true,
+          },
+        },
+      },
+  },
+],
   eslint: {
     mode: ESLINT_MODES.file,
   }
