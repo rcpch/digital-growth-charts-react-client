a57247186d03dae07710aa5bb1664146471439c7
anchit-chandran <anchit97123@gmail.com>
2023-07-24 16:28:30 +0100

    adds selectchoice menu button results table
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- src/components/subcomponents/ResultsDataTable.js
+++ src/components/subcomponents/ResultsDataTable.js
@@ -6,26 +6,32 @@ export const ResultsDataTable = ({ dataTitle, data }) => {
   return (
     <React.Fragment>
       <Header>
-        {dataTitle} (Corrected age results<Icon name="add circle" />)
+        {dataTitle} (Corrected age results
+        <Icon name="add circle" />)
       </Header>
-      <Table basic="very" celled collapsing compact>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Date of Measurement</Table.HeaderCell>
-            <Table.HeaderCell>Measurement</Table.HeaderCell>
-            <Table.HeaderCell>Age</Table.HeaderCell>
-            <Table.HeaderCell>Centile</Table.HeaderCell>
-            <Table.HeaderCell>SDS</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {data.map((measurement, index) => {
-            return (
-              <ResultsDataTableRow measurement={measurement} key={index} />
-            );
-          })}
-        </Table.Body>
-      </Table>
+
+      {data.length > 0 ? (
+        <Table basic="very" celled collapsing compact>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Date of Measurement</Table.HeaderCell>
+              <Table.HeaderCell>Measurement</Table.HeaderCell>
+              <Table.HeaderCell>Age</Table.HeaderCell>
+              <Table.HeaderCell>Centile</Table.HeaderCell>
+              <Table.HeaderCell>SDS</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {data.map((measurement, index) => {
+              return (
+                <ResultsDataTableRow measurement={measurement} key={index} />
+              );
+            })}
+          </Table.Body>
+        </Table>
+      ) : (
+        <h1>No data!</h1>
+      )}
     </React.Fragment>
   );
 };
--- src/components/subcomponents/ResultsSegment.js
+++ src/components/subcomponents/ResultsSegment.js
@@ -1,5 +1,6 @@
-import { Segment } from "semantic-ui-react";
+import { Segment, Select } from "semantic-ui-react";
 import { ResultsDataTable } from "./ResultsDataTable";
+import { useState } from "react";
 
 export const ResultsSegment = ({ apiResult, reference }) => {
   const heights = apiResult[reference].height;
@@ -7,25 +8,65 @@ export const ResultsSegment = ({ apiResult, reference }) => {
   const bmis = apiResult[reference].bmi;
   const ofcs = apiResult[reference].ofc;
 
+  const resultDataOptions = [
+    {
+      key: "heights",
+      text: "Heights",
+      value: "heights",
+    },
+    {
+      key: "weights",
+      text: "Weights",
+      value: "weights",
+    },
+    {
+      key: "bmis",
+      text: "BMIs",
+      value: "bmis",
+    },
+    {
+      key: "ofcs",
+      text: "OFCs",
+      value: "ofcs",
+    },
+  ];
+
+  const [choice, setChoice] = useState("");
+
+  function handleSelectChoice({value}) {
+    setChoice(value);
+  }
+
   return (
     <Segment>
-      {heights.length > 0 && (
+      <Select
+        placeholder="Select data to show"
+        options={resultDataOptions}
+        onChange={(e, choice) => handleSelectChoice(choice)}
+      ></Select>
+      {choice === "heights" && (
         <ResultsDataTable
           dataTitle={"Heights"}
           data={heights}
         ></ResultsDataTable>
       )}
-      {weights.length > 0 && (
+      {choice === "weights" && (
         <ResultsDataTable
           dataTitle={"Weights"}
           data={weights}
         ></ResultsDataTable>
       )}
-      {bmis.length > 0 && (
-        <ResultsDataTable dataTitle={"BMIs"} data={bmis}></ResultsDataTable>
+      {choice === "bmis" && (
+        <ResultsDataTable
+          dataTitle={"BMIs"}
+          data={bmis}
+        ></ResultsDataTable>
       )}
-      {ofcs.length > 0 && (
-        <ResultsDataTable dataTitle={"OFCs"} data={ofcs}></ResultsDataTable>
+      {choice === "ofcs" && (
+        <ResultsDataTable
+          dataTitle={"OFCs"}
+          data={ofcs}
+        ></ResultsDataTable>
       )}
     </Segment>
   );
