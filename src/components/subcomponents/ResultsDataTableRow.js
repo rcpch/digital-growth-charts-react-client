fb3b19f560a0e3eb1bd8c644e66dad9ed1a4b9fc
anchit-chandran <anchit97123@gmail.com>
2023-07-24 16:01:02 +0100

    reformats results datatable
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

---
+++ src/components/subcomponents/ResultsDataTableRow.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { Table } from "semantic-ui-react";
+import { units } from "../../functions/units";
+
+export const ResultsDataTableRow = (props) => {
+  const measurement = props.measurement;
+  return (
+    <Table.Row>
+      <Table.Cell>{measurement.measurement_dates.observation_date}</Table.Cell>
+      <Table.Cell>
+        {measurement.child_observation_value.observation_value}{" "}
+        {units(measurement.child_observation_value.measurement_method)}
+      </Table.Cell>
+      <Table.Cell>
+        {measurement.measurement_dates.corrected_calendar_age ??
+          measurement.plottable_data.centile_data.corrected_decimal_age_data
+            .corrected_gestational_age}
+      </Table.Cell>
+      <Table.Cell>
+        {measurement.measurement_calculated_values.corrected_centile}
+      </Table.Cell>
+      <Table.Cell>
+        {Math.round(
+          measurement.measurement_calculated_values.corrected_sds * 1000
+        ) / 1000}
+      </Table.Cell>
+    </Table.Row>
+  );
+};
--- src/components/subcomponents/ResultsSegment.js
+++ src/components/subcomponents/ResultsSegment.js
@@ -1,62 +1,32 @@
-import { Segment, Table } from "semantic-ui-react";
-import { TableBody } from "../subcomponents/TableBody";
+import { Segment } from "semantic-ui-react";
+import { ResultsDataTable } from "./ResultsDataTable";
 
-export const ResultsSegment = ({ apiResult, reference }) => (
-  <Segment>
-    <Table basic="very" celled collapsing compact>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell></Table.HeaderCell>
-          <Table.HeaderCell>Corrected Results</Table.HeaderCell>
-          <Table.HeaderCell>Chronological Results</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {apiResult[reference].height.length > 0 && (
-          <Table.Row>
-            <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Heights</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
-          </Table.Row>
-        )}
-        {apiResult[reference].height.length > 0 &&
-          apiResult[reference].height.map((measurement, index) => {
-            return <TableBody measurement={measurement} key={index} />;
-          })}
-        {apiResult[reference].weight.length > 0 && (
-          <Table.Row>
-            <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Weights</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
-          </Table.Row>
-        )}
-        {apiResult[reference].weight.length > 0 &&
-          apiResult[reference].weight.map((measurement, index) => {
-            return <TableBody key={index} measurement={measurement} />;
-          })}
-        {apiResult[reference].bmi.length > 0 && (
-          <Table.Row>
-            <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>BMIs</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
-          </Table.Row>
-        )}
-        {apiResult[reference].bmi.length > 0 &&
-          apiResult[reference].bmi.map((measurement, index) => {
-            return <TableBody key={index} measurement={measurement} />;
-          })}
-        {apiResult[reference].ofc.length > 0 && (
-          <Table.Row>
-            <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Head Circumferences</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
-          </Table.Row>
-        )}
-        {apiResult[reference].ofc.length > 0 &&
-          apiResult[reference].ofc.map((measurement, index) => {
-            return <TableBody key={index} measurement={measurement} />;
-          })}
-      </Table.Body>
-    </Table>
-  </Segment>
-);
+export const ResultsSegment = ({ apiResult, reference }) => {
+  const heights = apiResult[reference].height;
+  const weights = apiResult[reference].weight;
+  const bmis = apiResult[reference].bmi;
+  const ofcs = apiResult[reference].ofc;
+
+  return (
+    <Segment>
+      {heights.length > 0 && (
+        <ResultsDataTable
+          dataTitle={"Heights"}
+          data={heights}
+        ></ResultsDataTable>
+      )}
+      {weights.length > 0 && (
+        <ResultsDataTable
+          dataTitle={"Weights"}
+          data={weights}
+        ></ResultsDataTable>
+      )}
+      {bmis.length > 0 && (
+        <ResultsDataTable dataTitle={"BMIs"} data={bmis}></ResultsDataTable>
+      )}
+      {ofcs.length > 0 && (
+        <ResultsDataTable dataTitle={"OFCs"} data={ofcs}></ResultsDataTable>
+      )}
+    </Segment>
+  );
+};
