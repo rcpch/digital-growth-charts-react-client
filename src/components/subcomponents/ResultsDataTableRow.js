cf6a1de51eef61595418399374f832f6128edf1f
Simon Chapman <eatyourpeasapps@gmail.com>
2023-07-25 22:26:24 +0100

    move state to row animate icon with click

--- src/components/subcomponents/ResultsDataTableRow.js
+++ src/components/subcomponents/ResultsDataTableRow.js
@@ -1,9 +1,10 @@
-import * as React from "react";
-import { Table } from "semantic-ui-react";
+import { useState } from "react";
+import { Icon, Table } from "semantic-ui-react";
 import { units } from "../../functions/units";
 
-export const ResultsDataTableRow = (props) => {
-  const measurement = props.measurement;
+export const ResultsDataTableRow = ({ measurement }) => {
+  const [isCorrected, setIsCorrected] = useState(true);
+  const rotate = isCorrected ? "rotate(180deg)" : "rotate(0)";
   return (
     <Table.Row>
       <Table.Cell>{measurement.measurement_dates.observation_date}</Table.Cell>
@@ -12,17 +13,33 @@ export const ResultsDataTableRow = (props) => {
         {units(measurement.child_observation_value.measurement_method)}
       </Table.Cell>
       <Table.Cell>
-        {measurement.measurement_dates.corrected_calendar_age ??
-          measurement.plottable_data.centile_data.corrected_decimal_age_data
-            .corrected_gestational_age}
+        {isCorrected && measurement.measurement_dates.corrected_calendar_age
+          ? measurement.plottable_data.centile_data.corrected_decimal_age_data
+              .corrected_gestational_age
+          : measurement.plottable_data.centile_data.corrected_decimal_age_data
+              .chronological_gestational_age}
       </Table.Cell>
       <Table.Cell>
-        {measurement.measurement_calculated_values.corrected_centile}
+        {isCorrected
+          ? measurement.measurement_calculated_values.corrected_centile
+          : measurement.measurement_calculated_values.chronological_centile}
       </Table.Cell>
       <Table.Cell>
-        {Math.round(
-          measurement.measurement_calculated_values.corrected_sds * 1000
-        ) / 1000}
+        {isCorrected
+          ? Math.round(
+              measurement.measurement_calculated_values.corrected_sds * 1000
+            ) / 1000
+          : Math.round(
+              measurement.measurement_calculated_values.chronological_sds * 1000
+            ) / 1000}
+      </Table.Cell>
+      <Table.Cell>
+        <Icon
+          style={{ transform: rotate, transition: "all 0.2s linear" }}
+          name="refresh"
+          circular
+          onClick={() => setIsCorrected(!isCorrected)}
+        ></Icon>
       </Table.Cell>
     </Table.Row>
   );
