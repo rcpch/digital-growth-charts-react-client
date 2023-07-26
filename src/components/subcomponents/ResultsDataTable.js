683a596f64dfec1c00a0b544c0ae21cc7b478b00
anchit-chandran <anchit97123@gmail.com>
2023-07-26 11:34:59 +0100

    Revert "move state to row"
    This reverts commit cf6a1de51eef61595418399374f832f6128edf1f.

--- src/components/subcomponents/ResultsDataTable.js
+++ src/components/subcomponents/ResultsDataTable.js
@@ -1,31 +1,34 @@
+import * as React from "react";
 import { Table, Icon, Header } from "semantic-ui-react";
 import { ResultsDataTableRow } from "./ResultsDataTableRow";
 
 export const ResultsDataTable = ({ dataTitle, data }) => {
+
   return (
-    <>
+    <React.Fragment>
       <Header>
         {dataTitle} (Corrected age results <Icon name="add circle" />)
       </Header>
 
-      <Table basic="very" celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Date of Measurement</Table.HeaderCell>
-            <Table.HeaderCell>Measurement</Table.HeaderCell>
-            <Table.HeaderCell>Age</Table.HeaderCell>
-            <Table.HeaderCell>Centile</Table.HeaderCell>
-            <Table.HeaderCell>SDS</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {data.map((measurement, index) => {
-            return (
-              <ResultsDataTableRow measurement={measurement} key={index} />
-            );
-          })}
-        </Table.Body>
-      </Table>
-    </>
+      
+        <Table basic="very" celled >
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Date of Measurement</Table.HeaderCell>
+              <Table.HeaderCell>Measurement</Table.HeaderCell>
+              <Table.HeaderCell>Age</Table.HeaderCell>
+              <Table.HeaderCell>Centile</Table.HeaderCell>
+              <Table.HeaderCell>SDS</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {data.map((measurement, index) => {
+              return (
+                <ResultsDataTableRow measurement={measurement} key={index} />
+              );
+            })}
+          </Table.Body>
+        </Table>
+    </React.Fragment>
   );
 };
