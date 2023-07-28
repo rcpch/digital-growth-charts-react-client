e8f85ec347f0f553542d33e90300f7294ee9a156
anchit-chandran <anchit97123@gmail.com>
2023-07-28 11:46:21 +0100

    implements jest config setup + first basic test
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
+  plugins: [["@babel/plugin-transform-runtime", { regenerator: true }]],
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render } from "@testing-library/react";
+import '@testing-library/jest-dom'
 import App from "./App";
 
 test("renders RCPCH Growth Charts text", () => {
   const { getByText } = render(<App />);
-  const textElement = getByText(/Digital Growth Charts Demo/i);
+  const textElement = getByText("Dates");
   expect(textElement).toBeInTheDocument();
 });
