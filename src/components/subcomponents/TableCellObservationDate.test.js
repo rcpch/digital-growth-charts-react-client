087f82b57297a510489541002159cb27473a3dde
anchit-chandran <anchit97123@gmail.com>
2023-07-31 09:24:41 +0100

    adds first full test observation date text
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- src/components/subcomponents/TableCellObservationDate.test.js
+++ src/components/subcomponents/TableCellObservationDate.test.js
@@ -1,8 +1,23 @@
-import {render, screen} from '@testing-library/react'
-import TableCellObservationDate from './TableCellObservationDate'
+import { render, screen } from "@testing-library/react";
+import TableCellObservationDate from "./TableCellObservationDate";
+import { Table } from "semantic-ui-react";
 
-test('should show observation date',()=> {
-    render(<TableCellObservationDate measurement={{measurement_dates:{observation_date:"1760-04-10"}}}/>);
+test("should show correct observation date text in tr", () => {
+  const measurementProp = {
+    measurement_dates: { observation_date: "1760-04-10" },
+  };
 
-    const observationDate = screen.getByText("1760-04-10")
-})
+  render(
+    <Table>
+      <Table.Body>
+        <Table.Row>
+          <TableCellObservationDate measurement={measurementProp} />
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  );
+
+  const observationDate = screen.getByText("1760-04-10");
+
+  expect(observationDate.textContent).toBe("1760-04-10");
+});
