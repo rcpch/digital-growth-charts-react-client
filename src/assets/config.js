088da659931ad9a135863891647654f153a7290d
Raxxius <raxxman@gmail.com>
2024-02-26 19:23:48 +0000

    seperated constants from App.js to config.js

---
+++ src/assets/config.js
@@ -0,0 +1,30 @@
+export const panesBlueprint = [
+    {
+      menuItem: "Height",
+      measurementName: "height",
+      key: "Height",
+    },
+    {
+      menuItem: "Weight",
+      measurementName: "weight",
+      key: "Weight",
+    },
+    {
+      menuItem: "BMI",
+      measurementName: "bmi",
+      key: "BMI",
+    },
+    {
+      menuItem: "Head Circumference",
+      measurementName: "ofc",
+      key: "Head Circumference",
+    },
+  ];
+  
+  export const themeOptions = [
+    { key: "monochrome", value: "monochrome", text: "Monochrome" },
+    { key: "trad", value: "trad", text: "Traditional" },
+    { key: "tanner1", value: "tanner1", text: "Tanner 1" },
+    { key: "tanner2", value: "tanner2", text: "Tanner 2" },
+    { key: "tanner3", value: "tanner3", text: "Tanner 3" },
+  ];
