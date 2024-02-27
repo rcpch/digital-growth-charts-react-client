48d4ec92fe65cf18b8e2c73b49653508188d1a30
Raxxius <raxxman@gmail.com>
2024-02-27 10:48:20 +0000

    updated App to use new subcomponents

--- src/App.js
+++ src/App.js
@@ -14,19 +14,13 @@ import { themeOptions, panesBlueprint } from "./assets/config.js";
 
 // Semantic UI React
 import {
-  Grid,
-  Segment,
   Tab,
-  Button,
-  Message,
-  Container,
-  Checkbox,
 } from "semantic-ui-react";
 import createSemanticPanes from "./functions/semantic-ui-functions/createSemanticPanes.js";
 
 // Components
-import ThemeSelection from "./components/subcomponents/ThemeSelection.jsx";
 import ErrorModal from "./components/ErrorModal.jsx";
+import SemanticGrid from "./components/SemanticGrid/SemanticGrid.jsx";
 
 // Custom hooks
 import useErrorHandling from "./hooks/useErrorHandling.jsx";
@@ -38,7 +32,6 @@ import useRcpchApi from "./hooks/useRcpchApi";
 // Functions
 import ChangeTheme from "./functions/MeasurementSegment/handleChangeTheme";
 import MeasurementForm from "./components/MeasurementForm";
-import ResultsSegment from "./components/ResultsSegment.jsx";
 import "./index.css";
 import "./App.css";
 import FictionalChildForm from "./components/FictionalChildForm";
@@ -262,98 +255,29 @@ function App() {
     },
   ];
 
-  console.log(themeOptions, handleChangeTheme, theme.text)
+  console.log(themeOptions, handleChangeTheme, theme.text);
   // Core return
   return (
     <>
-      <Grid padded>
-        <Grid.Row>
-          <Grid.Column width={6}>
-            <Segment
-              textAlign={"center"}
-              color={mode === "fictional-child-data" ? "black" : null}
-            >
-              <Tab
-                key="measurementTabs"
-                panes={FormPanes}
-                menu={{
-                  attached: false,
-                  secondary: true,
-                  pointing: true,
-                }}
-                onTabChange={handleModeChange}
-                activeIndex={modeActiveIndex}
-              />
-            </Segment>
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Segment color={mode === "fictional-child-data" ? "black" : null}>
-              {flip ? (
-                <ResultsSegment apiResult={results} reference={reference} />
-              ) : (
-                <div>
-                  <TabPanes />
-                </div>
-              )}
-              <Grid verticalAlign="middle">
-                <Grid.Row columns={3}>
-                  <Grid.Column textAlign="left" width={4}>
-                    <Checkbox
-                      radio
-                      label="Clinician Advice"
-                      name="checkboxRadioGroup"
-                      value={0}
-                      checked={clinician}
-                      onChange={(e, data) => setClinician(!clinician)}
-                    />
-                    <Checkbox
-                      radio
-                      label="Child/Family Advice"
-                      name="checkboxRadioGroup"
-                      value={1}
-                      checked={!clinician}
-                      onChange={(e, data) => setClinician(!clinician)}
-                    />
-                  </Grid.Column>
-                  <Grid.Column textAlign="center" width={4}>
-                    <ThemeSelection
-                      options={themeOptions}
-                      onChange={handleChangeTheme}
-                      text={theme.text}
-                    />
-                  </Grid.Column>
-                  <Grid.Column textAlign="right" width={8}>
-                    <Button
-                      onClick={handleCentileSDS}
-                      color="black"
-                      disabled={flip}
-                    >
-                      {centile ? "Show SDS Chart" : "Show Centile Charts"}
-                    </Button>
-                    <Button
-                      disabled={!globalState.isDataPresent}
-                      className="selectUpperMargin"
-                      onClick={handleFlipResults}
-                    >
-                      {flip ? "Chart" : "Results"}
-                    </Button>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Container>
-            <Message
-              icon={"warning sign"}
-              header={"DISCLAIMER"}
-              content="This is for demonstration purposes only and is not for clinical use."
-              color="red"
-            />
-          </Container>
-        </Grid.Row>
-      </Grid>
+      <SemanticGrid
+        mode={mode}
+        flip={flip}
+        FormPanes={FormPanes}
+        handleModeChange={handleModeChange}
+        modeActiveIndex={modeActiveIndex}
+        results={results}
+        reference={reference}
+        clinician={clinician}
+        setClinician={setClinician}
+        TabPanes={TabPanes}
+        themeOptions={themeOptions}
+        handleChangeTheme={handleChangeTheme}
+        theme={theme}
+        handleCentileSDS={handleCentileSDS}
+        centile={centile}
+        globalState={globalState}
+        handleFlipResults={handleFlipResults}
+      />
       <ErrorModal
         title={errorModal.title}
         body={errorModal.body}
