0a5bfbd1189e351371d7e23bdfaa5aeafc79fff7
eatyourpeas <simon@eatyourpeas.co.uk>
2024-08-11 19:44:28 +0100

    vitify without typescript

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MeasurementSegment from './components/MeasurementSegment'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <MeasurementSegment />
+    </div>
+  );
+}
+
+export default App
--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders RCPCH Growth Charts text", () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/Digital Growth Charts Demo/i);
-  expect(textElement).toBeInTheDocument();
-});
