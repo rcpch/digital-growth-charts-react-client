f238812ccd10655eb1b07f4c9205b1a61c23c5b5
Michael Barton <michael@mbarton.cc>
2024-08-29 15:21:19 +0100

    Fix base path when serving under a folder in production

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+function getBasePath() {
+  // When deploying to GitHub pages it's served under a subdomain
+  // https://rcpch.github.io/digital-growth-charts-react-client/
+  //
+  // GITHUB_REPOSITORY is the full name: rcpch/digital-growth-charts-react-client
+
+  if(process.env["GITHUB_REPOSITORY"]) {
+    const [, repoName] = process.env['GITHUB_REPOSITORY'].split("/");
+
+    if(repoName) {
+      return `/${repoName}/`;
+    }
+  }
+
+  return '/';
+} 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: "0.0.0.0",
   port: 3000,
+  base: getBasePath()
 });
