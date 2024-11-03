e9237113fffb6d81f4f48ddbf35c7481536bf465
eatyourpeas <simon@eatyourpeas.co.uk>
2024-11-03 13:52:31 +0000

    refactors mid-parental height to capitals. adds client version to page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import  React from "react";
 import MeasurementSegment from "../components/MeasurementSegment";
+import packageJson from "../../package.json";
 
 const Home = () => {
+
   return (
     <div>
       <MeasurementSegment />
+      <small><i>RCPCH digital Growth Charts - Client : v{packageJson.version}, Charts: v{packageJson.dependencies["@rcpch/digital-growth-charts-react-component-library"]}.</i></small>
     </div>
   );
 };
