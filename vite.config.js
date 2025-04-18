c9255d04a2e0df65543b09338788ebe16cfe2884
eatyourpeas <simon@eatyourpeas.co.uk>
2025-04-18 23:06:11 +0100

    allow npm link in vite

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,12 @@ function getBasePath() {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    preserveSymlinks: true,
+  },
+  optimizeDeps: {
+    include: ["@rcpch/digital-growth-charts-react-component-library"],
+  },
   server: {
     host: "0.0.0.0",
     port: 3000,
