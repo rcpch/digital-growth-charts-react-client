27c0011735f04c6cd76ca4f811ab82a069310b18
Marcus Baw <marcus@marcusbaw.com>
2026-09-04 19:01:06 +0100

    fix: prevent mismatched example chart selections

--- src/components/MeasurementSegment.jsx
+++ src/components/MeasurementSegment.jsx
@@ -351,6 +351,7 @@ const MeasurementSegment=()=> {
       render: () => (
         <Tab.Pane key="presets">
           <Presets
+            key={`${reference}:${measurementMethod}`}
             globalState={globalState}
             updateGlobalState={updateGlobalState}
             handlePresetsSubmit={presetsDataSubmit}
